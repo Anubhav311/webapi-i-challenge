75a996adf585530bc3c02d0deda7d0de7ffad164
Anubhav311 <anubhavbhambri1@gmail.com>
2019-04-23 02:19:08 +0530

    Completed POST request

--- index.js
+++ index.js
@@ -2,10 +2,30 @@
 const express = require('express');
 const db = require('./data/db');
 const server = express();
+server.use(express.json());
 
-// server.get('/', (req, res) => {
-//     res.send("it's working")
-// })
+
+server.post('/api/users', (req, res) => {
+    const userInformation = req.body;
+    console.log(userInformation.name);
+
+    if (userInformation.name && userInformation.bio) {
+        db.insert(userInformation)
+        .then(user => {
+            res.status(201).json(user);
+        })
+        .catch(err => {
+            res.status(500).json({error: "There was an error while saving the user to the database"})
+        })
+
+    } else {
+        db.insert(userInformation)
+        .then()
+        .catch(err => {
+            res.status(400).json({errorMessage: "Please provide name and bio for the user."})
+        })
+    }
+})
 
 server.get('/api/users', (req, res) => {
     db.find()
