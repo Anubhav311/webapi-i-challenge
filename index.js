ad5c1e5daf5812c2e4e70074c171657222513411
Anubhav311 <anubhavbhambri1@gmail.com>
2019-04-23 01:47:24 +0530

    Completed get request

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // implement your API here
+const express = require('express');
+const db = require('./data/db');
+const server = express();
+
+server.get('/', (req, res) => {
+    res.send("it's working")
+})
+
+server.get('/api/users', (req, res) => {
+    db.find()
+        .then(user => {
+            res.status(200).json(user);
+        })
+        .catch(err => {
+            res.status(500).json({ error: "The users information could not be retrieved." })
+        })
+})
+
+server.listen(5000, () => {
+    console.log('API running on port 5000')
+})
